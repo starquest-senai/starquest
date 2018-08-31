cfe02078d352cb3028da781a055338bbb039762e
profjulimartins <42851511+profjulimartins@users.noreply.github.com>
2018-08-30 21:03:59 -0300

    Add files via upload
    Inclusão inicial de arquivos do meu projeto

---
+++ welcome.php
@@ -0,0 +1,35 @@
+﻿<html>
+
+<head>
+
+
+
+</head>
+
+<body>
+
+<html>
+<body>
+
+<?php
+echo phpversion();
+session_start();
+if(isset($_SESSION["nome"])){
+	
+	echo $_SESSION["nome"];
+}
+else
+{
+	header("location:index2.php");
+}
+//acabar sessao
+//unset($_SESSION["nome"]);
+
+?>
+</body>
+</html>
+
+
+</body>
+
+</html>
