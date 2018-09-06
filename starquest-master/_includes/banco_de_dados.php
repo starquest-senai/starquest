b69a43f1b40bf4b88af3cad4c59f8e13614bcdd4
Diego Moretti <42853488+moretti001@users.noreply.github.com>
2018-09-05 22:13:40 -0300

    Add files via upload

---
+++ starquest-master/_includes/banco_de_dados.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marcomauricio
+ * Date: 19/08/2018
+ * Time: 10:16
+ */
+
+function php_insert($pQuery){
+
+    $conexao = mysqli_connect("localhost","root","","db_starquest");
+    $dados = mysqli_query($conexao,$pQuery) or die ("ERRO de concexão do insert");
+
+    mysqli_close($conexao);
+
+    return $dados;
+}
+
+function select($pQuery){
+    //                          servidor        usuário             senha           nome do banco
+    $conexao = mysqli_connect("localhost","root","","db_starquest");
+    $dados = mysqli_query ($conexao,$pQuery)or die ("ERRO de conexão do select");
+
+    $arraySelect = array();
+
+    if ($dados -> num_rows > 0){
+
+        while ($row = $dados -> fetch_assoc()){
+            $arraySelect[] = $row;
+        }
+    }
+    else {
+        return 0;
+    }
+
+    mysqli_close($conexao);
+
+    return $arraySelect;
+
+}
+
+function php_update($pQuery){
+
+    $conexao = mysqli_connect("localhost","root","","db_starquest");
+    $dados = mysqli_query($conexao,$pQuery) or die ("ERRO de conexão do update");
+
+    $atualizadas = mysqli_affected_rows($conexao);
+
+    mysqli_close($conexao);
+
+    if($atualizadas > 0){
+
+        return $atualizadas." registro atualizado!";
+    }
+    else{
+        return "ERRO ao atualizar";
+    }
+}
+
+function php_delete($pQuery){
+
+    $conexao = mysqli_connect("localhost","root","","db_starquest");
+    $dados = mysqli_query($conexao,$pQuery) or die ("ERRO de conexão do delete");
+
+    $deletadas = mysqli_affected_rows($conexao);
+
+    mysqli_close($conexao);
+
+    if($deletadas > 0){
+
+        return $deletadas." registro atualizado!";
+    }
+    else{
+        return "ERRO ao deletar";
+    }
+}
