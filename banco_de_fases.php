078876c39494e2b4772404f6386dc9e07c80c4e5
MatheusMirandaPereira <42852960+MatheusMirandaPereira@users.noreply.github.com>
2018-09-26 22:04:44 -0300

    Add files via upload

--- banco_de_fases.php
+++ banco_de_fases.php
@@ -6,10 +6,7 @@ if(isset($_GET["jogadores_usuario"]))
 {
 	$return = select("SELECT * FROM progressos WHERE jogadores_usuario ='{$_GET['jogadores_usuario']}'");
 	
-	echo "INSERT INTO progressos (id, nivel, fase, pontuacao, jogadores_usuario) VALUES (DEFAULT, {$_GET['nivel']}, {$_GET['fase']}, {$_GET['pontuacao']}, {$_GET['jogadores_usuario']})";
-	
-	
-	php_insert("INSERT INTO progressos (id, nivel, fase, pontuacao, jogadores_usuario) VALUES ('', {$_GET['nivel']}, {$_GET['fase']}, {$_GET['pontuacao']}, {$_GET['jogadores_usuario']})");
+	php_update("UPDATE progressos SET id = DEFAULT, fase = '{$_GET['fase']}', pontuacao = '{$_GET['pontuacao']}' WHERE jogadores_usuario = '{$_GET['jogadores_usuario']}'");
 	
 	if($return != 0){
 		
@@ -17,6 +14,7 @@ if(isset($_GET["jogadores_usuario"]))
 		
 	}
 	else
+		
 		echo "Erro ao buscar nome!";
 }
 
