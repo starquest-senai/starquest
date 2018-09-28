99f01d8ec3b1eacbb892f8fea39e574635dbd28b
MatheusMirandaPereira <42852960+MatheusMirandaPereira@users.noreply.github.com>
2018-09-27 22:17:55 -0300

    Add files via upload

--- highscores.php
+++ highscores.php
@@ -12,7 +12,7 @@ include "includes/validacao_sessao.php";
     <link rel="stylesheet" type="text/css" href="assets/css/pagina_inicial.css"/>
     <link rel="stylesheet" type="text/css" href="assets/css/highscores.css"/>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
 
 </head>
 <body>
@@ -26,7 +26,37 @@ include "includes/validacao_sessao.php";
     <section id="corpo">
         <h1>HIGHSCORES</h1>
         <div id="highscores">
-
+			
+			<table class="table table-black">
+			  <thead>
+				<tr>
+				  <th id="titulo">Colocação</th>
+				  <th id="titulo">Jogador</th>
+				  <th id="titulo">Pontuação</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				
+				<?php
+				
+					for($i=1;$i<2;$i++){
+				
+						echo "
+						<tr>
+						  <th id='colocacao' style='text-align:center'>$i</th>
+						";
+					}
+				
+				?>
+				
+					<td id='colocacao' style='text-align:center'>Nome</td>
+					<td id='colocacao' style='text-align:center'>100</td>
+				</tr>
+				
+					
+			  </tbody>
+			</table>
+				
         </div>
         <a href="pagina_inicial.php" class="button orange shield glossy" style="padding-left: 1em; padding-right: 1em; margin: -7.5% 0% 3% 10%"> <img src="img/botao_voltar_30x34.png"> </a>
     </section>
@@ -39,6 +69,6 @@ include "includes/validacao_sessao.php";
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
